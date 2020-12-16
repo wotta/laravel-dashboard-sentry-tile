30c337944951aa7e236027b14a55899c9efa6799
Wouter van Marrum <wouter.van.marrum@protonmail.com>
2020-12-16 23:11:26 +0100

    Created a new SentryClient and added a test to verify if it works as we expect.

---
+++ src/Exceptions/NoClientTokenSet.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Wotta\SentryTile\Exceptions;
+
+use Exception;
+use Throwable;
+
+class NoClientTokenSet extends Exception
+{
+    public function __construct($code = 0, Throwable $previous = null)
+    {
+        parent::__construct(
+            "No sentry api token is set while trying to connect to sentry.\n
+            Please visit: https://sentry.io/settings/account/api/",
+            $code,
+            $previous);
+    }
+}
