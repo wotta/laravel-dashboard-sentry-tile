313bda7a9f2627bed42034886e4289561fd8ae1e
wotta <wotta@users.noreply.github.com>
2021-01-20 20:37:06 +0000

    Fix styling

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,7 +7,6 @@ use Illuminate\Foundation\Console\VendorPublishCommand;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\File;
-use Illuminate\Support\Facades\Schema;
 use Livewire\LivewireServiceProvider;
 use Wotta\SentryTile\SentryTileServiceProvider;
 
