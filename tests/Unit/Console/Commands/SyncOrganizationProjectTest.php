5a43447a94f0173efa8525cc7bf55c871c8feb3f
Wouter van Marrum <wouter.van.marrum@protonmail.com>
2021-01-23 17:56:15 +0100

    Created a test for the SyncOrganizationProject command.

---
+++ tests/Unit/Console/Commands/SyncOrganizationProjectTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Wotta\SentryTile\Tests\Unit\Console\Commands;
+
+use Wotta\SentryTile\Tests\TestCase;
+
+class SyncOrganizationProjectTest extends TestCase
+{
+    public function test_command_displays_information_that_is_stored(): void
+    {
+        $this->markTestIncomplete('Still need to implement this test');
+    }
+}
